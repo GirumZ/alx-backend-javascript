ff4fc97d01757066075b4f1d19ffa4a8a7ea08f4
GirumZ <girum.zebe@gmail.com>
2024-04-24 10:52:05 +0000

    [Update]: made the promis simple by removing reject and resolve

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,12 +1,4 @@
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const success = true;
-      if (success) {
-        resolve();
-      } else {
-        reject();
-      }
-    }, 2000);
+  return new Promise(() => {
   });
 }
