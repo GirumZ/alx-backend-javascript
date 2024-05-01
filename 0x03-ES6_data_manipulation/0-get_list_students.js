e22f4431310e66a3bcd0a460bed6128d6703d64e
GirumZ <girum.zebe@gmail.com>
2024-05-01 18:53:03 +0000

    [Bug fix]: Created separate array to be returned

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,8 @@
 export default function getListStudents() {
-  return [
+  const myArray = [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Colombia' },
     { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
+  return myArray;
 }
